ef3f7fdb70094a26187e9e1ceab89e31ac2e9904
removio <76274060+removio@users.noreply.github.com>
2020-12-20 07:41:23 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,229 @@
+var new_lang = {
+    "lt0": "XX",
+
+    "lt1": "Scapă de negi și papiloame rapid și în siguranță!",
+    
+    "lt2": "OFERTĂ SPECIALĂ",
+    
+    "lt3": "până la sfârșitul promoției au mai rămas <div class=\"form-header__timer-script\"><span class=\"hoursSH\">11</span>:<span class=\"minutesSH\">11</span>:<span class=\"secondsSH\">11</span></div>",
+    
+    "lt4": "Preț obișnuit",
+    
+    "lt5": "Preț promoțional",
+    
+    "lt6": "Comandă",
+    
+    "lt7": "EFECT",
+    
+    "lt8": "Eficacitatea împotriva <br>diferitelor tulpini <br>ale virusului papilomului",
+    
+    "lt9": "ore ",
+    
+    "lt10": "Distruge complet <br>agentul patogen <br>în primele 48 de ore",
+    
+    "lt11": "Complex",
+    
+    "lt12": "Blochează activitatea virusului <br>și activează protecția <br>imună",
+    
+    "lt13": "ÎNLĂTURAȚI PAPILOMELE ȘI PREVENIȚI CANCERUL PIELII! <br><span> XX</span>- SALVEAZĂ VIEȚI!",
+    
+    "lt14": "Combinația de ingrediente active elimină formațiunile pielii și previne degenerarea acestora în tumori canceroase. ",
+    
+    "lt15": "Papilome <br>pe gât",
+    
+    "lt16": "Papiloame <br>în zona <br>axilară ",
+    
+    "lt17": "Papiloame pe picioare <br>care au început <br>să regenereze ",
+    
+    "lt18": "Papilomul, <br>care s-a transformat în cancer ",
+    
+    "lt19": "Cauzele apariției papilomului",
+    
+    "lt20": "Papiloame sunt niște concrescențe pe piele cauzate de o infecție virală, care se numește: ”papilomavirus uman” sau HPV. Se transmite exclusiv de la persoană la persoană prin suprafețele deteriorate ale pielii. Perioada de incubație a acestei boli variază de la câteva zile la zeci de ani. De îndată ce corpul slăbește din cauza maladiilor, stresului, sarcinii la femei, papiloamele apar pe piele ",
+    
+    "lt21": "<span>XX</span> luptă cu toate tipurile de papiloame! ",
+    
+    "lt22": "<span style=\"color: #0158ff;\">Papiloame simple (vulgare)</span> Se formează de obicei pe membrele superioare.",
+    
+    "lt23": "<span style=\"color: #127abe;\">Papiloamele plate </span>Se răspândesc pe gât, față, <br>membre superioare. ",
+    
+    "lt24": "<span style=\"color: #0ba1e3;\">Papiloamele plantare </span>Afectează picioarele. ",
+    
+    "lt25": "<span style=\"color: #1dc1dd;\">Papiloame filiforme </span>Localizare: gât, față, zona inghinală și<br> zone axilare ",
+    
+    "lt26": "<span style=\"color: #e2000f;\">Candiloamele</span> Numite și negi genitali, <br>sunt deosebit de periculoase.",
+    
+    "lt27": "SCĂPAȚI DE ELE CÂT MAI DEGRABĂ!",
+    
+    "lt28": "Gelul <span>XX </span>este o modalitate simplă și eficientă de a scăpa de papiloame și negi! ",
+    
+    "lt29": "Suprimă activitatea celulelor virale, pătrunzând în straturile profunde ale dermei",
+    
+    "lt30": "Îndepărtează nedureros neoplasmele, prevenind dezvoltarea în continuare a papiloamelor",
+    
+    "lt31": "Reface zonele deteriorate ale pielii",
+    
+    "lt32": "Crește rezistența la viruși, infecții fungice, previne dezvoltarea cancerului",
+    
+    "lt33": "OFERTĂ SPECIALĂ",
+    
+    "lt34": "până la sfârșitul promoției au mai rămas <div class=\"form-header__timer-script\"><span class=\"hoursSH\">11</span>:<span class=\"minutesSH\">11</span>:<span class=\"secondsSH\">11</span></div>",
+    
+    "lt35": "Preț obișnuit",
+    
+    "lt36": "Preț promoțional",
+    
+    "lt37": "Comandă",
+    
+    "lt38": "COMANDĂ",
+    
+    "lt39": "Gelul <span>XX </span>este o modalitate simplă și eficientă de a scăpa de papiloame și negi! ",
+    
+    "lt40": "Ulei de ghimbir",
+    
+    "lt41": "Posedă puternice proprietăți antibacteriene și antiinflamatoare pentru a preveni răspândirea virusului",
+    
+    "lt42": "Extract de Sýmphytum",
+    
+    "lt43": "Utilizat în medicină pentru tratarea bolilor<br> inflamatorii. Dezintegrează <br>rapid și nedureros negii și papiloamele",
+    
+    "lt44": "Extract de melisă",
+    
+    "lt45": "Are efect antiviral pronunțat. Îmbunătățește producția de colagen și elastină, proteine esențiale pentru o piele sănătoasă ",
+    
+    "lt46": "Tocoferol ",
+    
+    "lt47": "Are proprietăți antiinflamatorii și antiseptice, ajută la refacerea epidermei, hrănește pielea cu vitamine și microelemente",
+    
+    "lt48": "Uleiul de iarbă de lămâie",
+    
+    "lt49": "Datorită compoziției sale chimice bogate, este eficient în tratarea cicatricilor și petelor. Elimină inflamația, îmbunătățește imunitatea locală ",
+    
+    "lt50": "Ulei de arbore de ceai",
+    
+    "lt51": "Oprește nutriția papiloamelor și a negilor, asigurând moartea în siguranță a neoplasmelor cutanate și prevenind apariția altora noi",
+    
+    "lt52": "Scorțișoara",
+    
+    "lt53": "Are un efect antiviral puternic, previne apariția cancerului, oprește diviziunea celulelor patogene ",
+    
+    "lt54": "Nicotinamidă",
+    
+    "lt55": "Îmbunătățește imunitatea locală Are un efect bactericid și de vindecare a rănilor, oferind un tratament rapid și nedureros. ",
+    
+    "lt56": "<span>XX </span>- noua ta viață fără papiloame și negi!",
+    
+    "lt57": "Impact țintit",
+    
+    "lt58": "Distruge papiloamele din rădăcină",
+    
+    "lt59": "Eliberarea de virus ",
+    
+    "lt60": "Fără urme și nedureros ",
+    
+    "lt61": "Acțiune delicată",
+    
+    "lt62": "Nu deteriorează pielea,<br> regenerează țesutul",
+    
+    "lt63": "COMANDĂ",
+    
+    "lt64": "Studiile clinice",
+    
+    "lt65": "În 2019, specialiștii de la Institutul de Dermatovenereologie au efectuat studii clinice cu <span>XX</span> pe un grup de 658 de voluntari care sufereau de HPV și care aveau papiloame și negi pe corp. Au fost obținute următoarele date:",
+    
+    "lt66": "Îndepărtarea formațiunilor externe (papiloame, condiloame, negi) ",
+    
+    "lt67": "Eliminarea virusului recidivant (rezultate după 3 luni) ",
+    
+    "lt68": "99% dintre cei testați nu au avut efecte secundare ",
+    
+    "lt69": "Opinia expertului",
+    
+    "lt70": "”Aproape fiecare persoană are papiloame și negi. Acestea sunt astfel de concrescențe benigne pe corp. Dar insidiositatea lor constă în faptul că mai devreme sau mai târziu pot deveni maligne și pot duce la boli oncologice. Prin urmare, papiloamele trebuie îndepărtate, chiar dacă nu vă deranjează din punct de vedere cosmetic. <br>Pacienților mei, recomand utilizarea gelului <span>XX</span>. Acesta este cel mai eficient și mai sigur remediu disponibil în acest moment. XX combate papiloamele și negii în cele mai adânci straturi ale pielii, eliminând complet neoplasmele nedorite.",
+    
+    "lt71": "Florian Rădoi.<br>Medic dermatovenerolog",
+    
+    "lt72": "Recenziile clienților",
+    
+    "lt73": "Mihai, 50 de ani",
+    
+    "lt74": "”Am avut papiloame încă din copilărie, dar nu le-am acordat prea multă importanță. În general, am crezut că acestea erau un fel de alunițe și nu era deloc necesar să le elimini. Soția mea mi-a deschis ochii la faptul că acestea nu sunt deloc alunițe și pot duce la formarea cancerului. La insistența ei, am mers la un medic care mi-a prescris gel <span>XX</span>. Papiloamele au trecut rapid și nedureros, fără a lăsa vreo urmă.”",
+    
+    "lt75": "Denisa, 35 ani",
+    
+    "lt76": "”Prietenul meu m-a infectat cu negi genitali. Am mers împreună pentru a primi tratament la un medic care a spus că virusul nostru este extrem de oncogen, adică cu o probabilitate ridicată să poată duce la dezvoltarea cancerului. Ne-am speriat pe bune! Ni s-a prescris o mulțime de pastile, dar efectul a fost slab. Despre <span>XX</span> am aflat de la o prietenă, când i-am împărtășit problema, am decis să încerc. Timp de o lună am aplicat acest gel pe zonele problematice, totul a trecut destul de rapid. Două luni mai târziu, am trecut testele și rezultatele au fost negative!”",
+    
+    "lt77": "Margareta, 43 de ani",
+    
+    "lt78": "”Am negi acum vreo cinci ani. De îndată ce am încercat să mă lupt cu ele, am încercat diverse unguente, cauterizare, îndepărtare chirurgicală. Nimic nu a ajutat. Soluția am găsit-o din întâmplare pe Internet - am dat peste un articol despre <span>XX</span>. L-am comandat, l-am încercat. După două săptămâni, am observat că negii au început să devină negri și apoi au căzut complet. În decurs de o lună de la prima utilizare, pielea a devenit absolut curată, nu a rămas nici o cicatrice”.",
+    
+    "lt79": "Mihai, 50 de ani",
+    
+    "lt80": "”Am avut papiloame încă din copilărie, dar nu le-am acordat prea multă importanță. În general, am crezut că acestea erau un fel de alunițe și nu era deloc necesar să le elimini. Soția mea mi-a deschis ochii la faptul că acestea nu sunt deloc alunițe și pot duce la formarea cancerului. La insistența ei, am mers la un medic care mi-a prescris gel <span>XX</span>. Papiloamele au trecut rapid și nedureros, fără a lăsa vreo urmă.”",
+    
+    "lt81": "Denisa, 35 ani",
+    
+    "lt82": "”Prietenul meu m-a infectat cu negi genitali. Am mers împreună pentru a primi tratament la un medic care a spus că virusul nostru este extrem de oncogen, adică cu o probabilitate ridicată să poată duce la dezvoltarea cancerului. Ne-am speriat pe bune! Ni s-a prescris o mulțime de pastile, dar efectul a fost slab. Despre <span>XX</span> am aflat de la o prietenă, când i-am împărtășit problema, am decis să încerc. Timp de o lună am aplicat acest gel pe zonele problematice, totul a trecut destul de rapid. Două luni mai târziu, am trecut testele și rezultatele au fost negative!”",
+    
+    "lt83": "Margareta, 43 de ani",
+    
+    "lt84": "”Am negi acum vreo cinci ani. De îndată ce am încercat să mă lupt cu ele, am încercat diverse unguente, cauterizare, îndepărtare chirurgicală. Nimic nu a ajutat. Soluția am găsit-o din întâmplare pe Internet - am dat peste un articol despre <span>XX</span>. L-am comandat, l-am încercat. După două săptămâni, am observat că negii au început să devină negri și apoi au căzut complet. În decurs de o lună de la prima utilizare, pielea a devenit absolut curată, nu a rămas nici o cicatrice”.",
+    
+    "lt85": "Mihai, 50 de ani",
+    
+    "lt86": "”Am avut papiloame încă din copilărie, dar nu le-am acordat prea multă importanță. În general, am crezut că acestea erau un fel de alunițe și nu era deloc necesar să le elimini. Soția mea mi-a deschis ochii la faptul că acestea nu sunt deloc alunițe și pot duce la formarea cancerului. La insistența ei, am mers la un medic care mi-a prescris gel <span>XX</span>. Papiloamele au trecut rapid și nedureros, fără a lăsa vreo urmă.”",
+    
+    "lt87": "XX",
+    
+    "lt88": "Scapă de negi și papiloame rapid și în siguranță!",
+    
+    "lt89": "OFERTĂ SPECIALĂ",
+    
+    "lt90": "până la sfârșitul promoției au mai rămas <div class=\"form-header__timer-script\"><span class=\"hoursSH\">11</span>:<span class=\"minutesSH\">11</span>:<span class=\"secondsSH\">11</span></div>",
+    
+    "lt91": "Preț obișnuit",
+    
+    "lt92": "Preț promoțional",
+    
+    "lt93": "Comandă",
+    
+    "lt94": "EFECT",
+    
+    "lt95": "Eficacitatea împotriva <br>diferitelor tulpini <br>ale virusului papilomului",
+    
+    "lt96": "ore ",
+    
+    "lt97": "Distruge complet <br>agentul patogen <br>în primele 48 de ore",
+    
+    "lt98": "Complex",
+    
+    "lt99": "Blochează activitatea virusului <br>și activează protecția <br>imună",
+    
+    "lt100": "COMANDĂ în 3 pași",
+    
+    "lt101": "I PAS",
+    
+    "lt102": "Lăsași o solicitare <br>pe site-ul nostru",
+    
+    "lt103": "AL II-LEA PAS",
+    
+    "lt104": "Operatorul vă va contacta <br>pentru <br>confirmarea comenzii",
+    
+    "lt105": "AL III-LEA PAS",
+    
+    "lt106": "Achitarea se face online sau<br> la recepționarea<br> comenzii"
+    
+
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
